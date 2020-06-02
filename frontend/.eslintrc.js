6ca6c43890dbb4c08d541d78a88209aa411299e0
Otavio Augusto <tavioalves@gmail.com>
2020-06-02 20:24:36 -0300

    chore: Remove unnecessary files of CRA from frontend project, add eslint as dev dependency, create eslint rules from airbnb and add some personal ones

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    indent: [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    quotes: [
+      "error",
+      "double"
+    ],
+    semi: [
+      "error",
+      "always"
+    ],
+    "react/jsx-filename-extension": "off"
+  }
+};
