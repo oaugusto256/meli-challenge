e333f0a70e385a769c82b7000d2fa527bab8b338
Otavio Augusto <tavioalves@gmail.com>
2020-06-03 00:40:52 -0300

    styles: Add new colors on tailwind and set some refactors on SearchBox component

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -8,7 +8,7 @@ const SearchBox = () => (
     <header className="container mx-auto flex items-center px-4 h-16">
       <img src={MercadoLibreLogo} alt="Mercado Libre Logo" />
       <input placeholder="Nunca dejes de buscar" className="flex-1 h-10 ml-8 p-4 rounded-sm rounded-r-none" type="text" />
-      <button className="bg-grey flex items-center h-10 p-3 rounded-sm rounded-l-none" type="button">
+      <button className="bg-grey-100 flex items-center h-10 p-3 rounded-sm rounded-l-none" type="button">
         <img src={SearchIcon} alt="Search icon" />
       </button>
     </header>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   theme: {
     colors: {
       yellow: "#ffe600",
-      grey: "#eeeeee",
+      blue: "#3483fa",
+      grey: {
+        100: "#eeeeee",
+        500: "#999999",
+        700: "#666666",
+        900: "#333333",
+      },
     },
   },
 };
