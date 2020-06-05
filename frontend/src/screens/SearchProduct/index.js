289a21a772205501584083b3f3d82c14b13e48f6
Otavio Augusto <tavioalves@gmail.com>
2020-06-05 01:08:23 -0300

    feat: Add ListItems component first draft as well as first notification markup and general feat related to it

--- frontend/src/screens/SearchProduct/index.js
+++ frontend/src/screens/SearchProduct/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 
-import { SearchHeader } from "../../components";
+import { useSelector } from "react-redux";
 
-const SearchProduct = () => (
-  <>
-    <SearchHeader />
-  </>
-);
+import { SearchHeader, ListItems, Container } from "../../components";
+
+const SearchProduct = () => {
+  const { items, notification } = useSelector((state) => state.app);
+
+  return (
+    <>
+      <SearchHeader />
+      <section className="bg-grey-100 h-full">
+        <Container>
+          <div className="p-8">
+            <div className="box-border rounded-md border-8 border-white">
+              {notification ? (
+                <div className="bg-white">
+                  <h1 className="p-8 text-grey-900 text-center text-xl">{notification}</h1>
+                </div>
+              ) : (
+                  <ListItems items={items} />
+                )}
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
 
 export default SearchProduct;
